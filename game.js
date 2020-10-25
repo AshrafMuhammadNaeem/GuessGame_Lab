7e750bcbbb5610a32719d73482a3c2bc2c749dde
Naeem Ashraf <naeemashraf.tud@gmail.com>
2020-10-25 21:05:33 +0500

    2nd game code with guess limit in for loop

--- game.js
+++ game.js
@@ -1,12 +1,25 @@
 // program data:
-var number = window.prompt("Enter a random number");
+var number = 7;
+// create variable to store guess number given in for loop code
+var guess;
+// create a variable name limit to give user try limit
+var limit = 5;
+// give the user try of 5 with for loop by creating another variable i to relate with limit.
+for ( i = 1; i < limit; i++) {
 // prompt user for the guess
-var guess = window.prompt("Guess the number and enter the number")
+   guess = window.prompt("Guess the number and enter the number")
+
+
 // if correct: let the user know they won
 if (number == guess) {
     document.write("wow!, your guess is corrent You WON")
-    // if incorrect: let the user know
+    // if incorrect: let the user know and about remaing tries
 } else {                                                     
     alert("Your guess is wrong")
-    document.write("Wrong Guess")
+     alert("try again, \nTries remaining " + (limit- i) );
+    
 }
+}
+
+    
+
