7ba76f9c3012b3a2f1f2261fb3cb336be7a24537
Naeem Ashraf <naeemashraf.tud@gmail.com>
2020-10-28 12:43:07 +0500

    10th game code , successfully entered duplicate guess functionality

--- game.js
+++ game.js
@@ -9,20 +9,26 @@ var guesses = [];
 var won = false;
 // give the user try of 5 with for loop by creating another variable i to relate with limit 
 for ( i = 1; i < limit; i++) {
-    guess = prompt("Guess a number by entering a number:");
+    
+        guess = prompt("Guess a number by entering a number:");
+    
+    
 // if correct: let the user know they won
 if (number == guess) {
-    document.write("wow!, your guess is corrent You WON")
+    document.write("wow!, your guess is correct You WON")
     won = true;
     // enter break otherwise loop will run till limit.
     break;
     // if incorrect: let the user know and about remaing tries
-} else {   
-    guesses[i] = guess;
-    
-     alert("Your guess is wrong.  \nYou have guessed:" + guesses.toString() , "\nTries remaining " + (limit- i) );
-    
 } 
+else if (isPreviousGuess(guess)) {
+    alert("duplicate number try again")
+}  
+ else {   
+    guesses[i] = guess;
+    alert("Your guess is wrong.  \nYou have guessed:" + guesses.toString() , "\nTries remaining " + (limit- i) );
+
+}
 }
 if (!won) { 
     document.write("Sorry You are out of tries. Game Over. <br> Correct number was:"  + number);
@@ -30,7 +36,7 @@ if (!won) {
 }
 function isPreviousGuess() {
     for ( var i = 1; i < guesses.length; i++) {
-     if (guesses[i] = guess) {
+     if (guesses[i] == guess) {
         return true;
      }
         
